c18dbd124dda3518018309c65c07a4b2a0bf15c2
david <dshlaifer@gmail.com>
2025-11-07 18:11:10 -0500

    question1 part 1

---
+++ part1.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class part1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class part1
+{
+    // instance variables - replace the example below with your own
+    private int price;
+
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int price()
+    {
+        if (price > 100) {
+        System.out.println("expensive");
+    }else { System.out.println("cheap");
+        
+    }
+    return price;
+    }
+}
