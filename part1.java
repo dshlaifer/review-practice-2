fcfa9ee75ee0aca907048c7d14d9a3524b683ca2
david <dshlaifer@gmail.com>
2025-11-11 16:26:16 -0500

    question 6

--- part1.java
+++ part1.java
@@ -60,13 +60,8 @@ public class part1
     }
     
     public void printNumbers() {
-        
-        int i = 1;
-        while (1 <= 10) {
-        System.out.println(i);
-        i++;
-        
-    }
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 } 
 }
