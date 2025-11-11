0a73d4d3c6bd0765cfecadfed7506f8daaf30088
david <dshlaifer@gmail.com>
2025-11-11 16:35:40 -0500

    question 8

--- part1.java
+++ part1.java
@@ -59,10 +59,12 @@ public class part1
 
     }
     
-    public void evenNumbers() {
+    public void evenNumbers(int a, int b) {
 
-        for (int i = 2; i <= 10; i += 2) {
-            System.out.println(i);
+        for (int i = a; i <= b; i ++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
 
         }
 
